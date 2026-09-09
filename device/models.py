e298845c72a5f1b77b4dd3664c7846c3f7556ecb
unknown <ChenBoTang>
2026-09-09 17:21:14 +0800

    Update Django cleaning dispatch system

--- device/models.py
+++ device/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ESP32Device(models.Model):
+    device_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, default="LOLIN32")
+    last_seen = models.DateTimeField(null=True, blank=True)
+    led_state = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def is_online(self):
+        if not self.last_seen:
+            return False
+
+        from django.utils import timezone
+        from datetime import timedelta
+
+        return timezone.now() - self.last_seen < timedelta(seconds=30)
